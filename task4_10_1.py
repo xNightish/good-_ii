26312d4a01449ffe0a11a71ce5bebdd37ef7d2f5
xNightish <pandaubiica@gmail.com>
2024-11-04 15:43:45 +0300

    изменена формела

--- task4_10_1.py
+++ task4_10_1.py
@@ -7,7 +7,7 @@ FP = [18, 21, 15, 17]
 FN = [8, 11, 9, 5]
 
 # Рассчет precision и recall, используя микро-усреднение
-precision = np.mean([tp / (tp + fp) for tp, fp in zip(TP, FP)])
-recall = np.mean([tp / (tp + fn) for tp, fn in zip(TP, FN)])
+precision = np.mean(TP) / (np.mean(TP) + np.mean(FP))
+recall = np.mean(TP) / (np.mean(TP) + np.mean(FN))
 
 print(f'precision = {precision}, recall = {recall}')
