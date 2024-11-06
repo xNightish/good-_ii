fc9b0609feaf553d5c834b865c6aef4652234454
xNightish <pandaubiica@gmail.com>
2024-11-06 19:25:10 +0300

    added graph

--- task5_5_1.py
+++ task5_5_1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Установим случайное начальное состояние для воспроизводимости
 np.random.seed(0)
@@ -29,11 +30,37 @@ ma = np.array([np.random.normal(M, np.sqrt(D / 10), 2) for _ in range(K)])
 # Алгоритм k-средних
 for n in range(10):  # фиксированное количество итераций
     # Присвоение точек к ближайшему центру кластера и обновление центров кластеров
-    y = np.argmin(np.abs(data_x[:, np.newaxis] - ma).sum(axis=2), axis=1)
+    y = np.argmin(np.sum(np.abs(data_x[:, np.newaxis] - ma), axis=2), axis=1)
     ma = np.array([data_x[y == m].mean(axis=0) for m in range(K)])
 
 # Преобразование результата
 X = [list(map(tuple, data_x[y == m])) for m in range(K)]
 
-# Вывод результата
-print(*X, sep='\n')
+# Визуализация
+plt.figure(figsize=(10, 8))
+
+# Создание сетки для предсказания кластеров
+x_min, x_max = data_x[:, 0].min() - 10, data_x[:, 0].max() + 10
+y_min, y_max = data_x[:, 1].min() - 10, data_x[:, 1].max() + 10
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 1), np.arange(y_min, y_max, 1))
+
+# Предсказание кластеров для каждой точки на сетке
+Z = np.argmin(np.sum(np.abs(np.c_[xx.ravel(), yy.ravel()] - ma[:, np.newaxis]), axis=2), axis=0)
+Z = Z.reshape(xx.shape)
+
+
+# Отображение точек данных
+for i in range(K):
+    cluster_points = np.array(X[i])
+    plt.scatter(cluster_points[:, 0], cluster_points[:, 1], label=f'Кластер {i+1}')
+
+# Отображение центров кластеров
+plt.scatter(ma[:, 0], ma[:, 1], color='black', marker='X', s=200, label='Центры кластеров')
+
+# Настройка графика
+plt.title('Кластеризация k-средних')
+plt.xlabel('Координата X')
+plt.ylabel('Координата Y')
+plt.legend()
+plt.grid()
+plt.show()
