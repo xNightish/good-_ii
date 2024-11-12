cdc57b99aef3a5cf379d48bd20b89b53cd15e10a
xNightish <pandaubiica@gmail.com>
2024-11-12 15:58:20 +0300

    created

---
+++ task7_4_1.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def act_sigma(z):
+    return z
+
+
+w = [2, -5, -7]
+
+
+
+
+
---
+++ task7_4_2.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+def act_sigma(z):
+    return z
+
+
+w = [5/9, 160/9]
